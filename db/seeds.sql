0df18205b5984bd6343d6eb4683e892e75c60f50
sshahram <shirin.shahram23@gmail.com>
2021-04-18 23:14:59 -0700

    clean up database

--- db/seeds.sql
+++ db/seeds.sql
@@ -16,12 +16,12 @@ VALUES
 ('Mover', 100, 4),
 ('Material Handler', 150.5, 5),
 ('Operator', 105.6, 6),
-('Engineer', 400, 7);
+('Mechanical Engineer', 400, 7);
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
 ('John', 'Lin', 7, NULL),
 ('Carlos', 'Dominguez', 4, 1),
-('Jasan', 'Hernandez', 6, 2),
+('Jason', 'Hernandez', 6, 2),
 ('Elizabeth', 'Tracy', 6, 1),
 ('Jeff', 'Ferrel', 7, NULL);
